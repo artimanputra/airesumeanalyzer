3015a80634d2cc6e53a5e0e7370e452a0c868a33
aartimanputra20 <aartimanputra20@gmail.com>
2026-01-07 09:22:04 +0000

    added new components

---
+++ app/components/ATS.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const ATS=()=>{
+    return(
+        <>ATS</>
+    )
+}
+
+export default ATS;
---
+++ app/components/Summary.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Summary=()=>{
+return(
+    <>Summary</>
+)
+}
+
+export default Summary;
